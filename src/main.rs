bc012821da46bed3715cf8adb74734412c3b7dfb
Simon Schneemelcher <s.schneemelcher@tu-bs.de>
2021-05-15 18:58:51 +0200

    added dataloader and changed bias

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 extern crate nalgebra as na;
 extern crate rand;
 
+use csv::StringRecord;
 use na::SMatrix;
 use rand::{thread_rng, Rng};
 use rand::distributions::Uniform;
 
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::fs::File;
+use std::process;
 
-type Matrix1x310f = SMatrix<f32, 1, 310>;
-type Matrix310x128f = SMatrix<f32, 310, 128>;
+
+type Matrix1x784f   = SMatrix<f32, 1, 784>;
+type Matrix784x128f = SMatrix<f32, 784, 128>;
 type Matrix128x64f  = SMatrix<f32, 128, 64>;
 type Matrix64x1f    = SMatrix<f32, 64, 1>;
+type Matrix1x128f    = SMatrix<f32,1, 128>;
+type Matrix1x64f    = SMatrix<f32, 1, 64>;
+type Record         = (i32, Vec<f32>);
 
 
 fn sigmoid(x: f32) -> f32 {
@@ -22,23 +32,22 @@ fn sigmoid(x: f32) -> f32 {
   }
 }
 
-
 struct NeuralNetwork {
-    layer1: Matrix310x128f,
+    layer1: Matrix784x128f,
     layer2: Matrix128x64f,
     layer3: Matrix64x1f,
-    bias1: f32,
-    bias2: f32,
+    bias1:  Matrix1x128f,
+    bias2:  Matrix1x64f,
 }
 
 impl NeuralNetwork {
 
-    fn predict(&mut self, input: Matrix1x310f) -> f32 {
+    fn predict(&mut self, input: Matrix1x784f) -> f32 {
 
-        let mut l1 = input * self.layer1;
-        l1.apply(|x| {sigmoid(x + self.bias1)});
-        let mut l2 = l1 * self.layer2;
-        l2.apply(|x| {sigmoid(x + self.bias2)});
+        let mut l1 = input * self.layer1 + self.bias1;
+        l1.apply(|x| {sigmoid(x)});
+        let mut l2 = l1 * self.layer2 + self.bias2;
+        l2.apply(|x| {sigmoid(x)});
         let mut l3 = l2 * self.layer3;
         l3.apply(|x| {sigmoid(x)});
         return l3.sum(); 
@@ -46,22 +55,50 @@ impl NeuralNetwork {
 
 }
 
-fn CreateNet() -> NeuralNetwork {
+fn create_net() -> NeuralNetwork {
         let mut rng = thread_rng();
 
         let mut nn = NeuralNetwork {
-        layer1: Matrix310x128f::from_iterator((&mut rng).sample_iter(Uniform::from(-0.5..0.5))),
+        layer1: Matrix784x128f::from_iterator((&mut rng).sample_iter(Uniform::from(-0.5..0.5))),
         layer2: Matrix128x64f::from_iterator((&mut rng).sample_iter(Uniform::from(-0.5..0.5))),
         layer3: Matrix64x1f::from_iterator((&mut rng).sample_iter(Uniform::from(-0.5..0.5))),
-        bias1: rng.gen(),
-        bias2: rng.gen()};
+        bias1:  Matrix1x128f::from_iterator((&mut rng).sample_iter(Uniform::from(-0.5..0.5))),
+        bias2:  Matrix1x64f::from_iterator((&mut rng).sample_iter(Uniform::from(-0.5..0.5))),
+        };
         return nn;
 }
 
 
+fn load_data() -> Result<(Vec<Matrix1x784f>, Vec<i32>), Box<dyn Error>> {
+    let file_path = get_arg()?;
+    let file = File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    let mut x_train: Vec<Matrix1x784f> = [].to_vec();
+    let mut y_train: Vec<i32> = [].to_vec();
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        y_train.push(record.0);
+        x_train.push(Matrix1x784f::from_vec(record.1));
+    }
+    Ok((x_train, y_train))
+}
+
+fn get_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected an argument")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
 fn main() {
-    let mut rng = thread_rng();
-    let mut x_train = Matrix1x310f::from_iterator((&mut rng).sample_iter(Uniform::from(-0.5..0.5)));
-    let mut nn = CreateNet();
-    println!("{:#?}", nn.predict(x_train));
+    match load_data() {
+        Err(err) => {println!("{}", err);
+                    process::exit(1);},
+        Ok((x_train, y_train)) => {
+    
+            let mut rng = thread_rng();
+            let mut nn = create_net();
+            println!("{:#?}", nn.predict(x_train[0]));
+        }
+    }
 }
